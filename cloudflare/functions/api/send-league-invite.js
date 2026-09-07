f9743f0a53e5be8f5ab479fe749672f30334e355
rodrigocostaalves <rodrigo.costaalves@yahoo.com.br>
2026-09-07 15:57:31 -0300

    Add files via upload

--- cloudflare/functions/api/send-league-invite.js
+++ cloudflare/functions/api/send-league-invite.js
@@ -1,7 +1,7 @@
 import { buildPushPayload } from '@block65/webcrypto-web-push';
 import { sendFcmNotification } from './fcm.js';
 
-const APP_URL = 'https://meu-placar-tenis.pages.dev/site/';
+const APP_URL = 'https://deuce-score-site.pages.dev/';
 
 function json(data, status = 200) {
   return new Response(JSON.stringify(data), { status, headers: { 'Content-Type': 'application/json' } });
